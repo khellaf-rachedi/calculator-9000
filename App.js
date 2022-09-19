4d0ecabc7ccd7595469628f13244c2f87ceac5dc
khellaf-rachedi <91748769+khellaf-rachedi@users.noreply.github.com>
2022-09-19 02:31:10 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+// import BeautifulScreen from './composants/BeautifulScreen.js';
+import Calculator from './composants/Calculator';
+
+
+function App () {
+
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
+
